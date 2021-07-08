8a489a67aa5357ae7a9f0efa3e1bb3c89d77f486
tiVaibhav7704 <vaibhavtiwari7704@gmai.com>
2021-07-09 00:35:45 +0530

    button

---
+++ app/src/main/java/com/example/a91853/firstapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a91853.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+     Button b1;
+     EditText e1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1= e1.getText().toString();
+                Toast.makeText(MainActivity.this, s1 , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
